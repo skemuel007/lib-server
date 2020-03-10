14f085fe5eb14752e9d9acfac553fa7aae62a0ac
skemuel007 <skemuel007@gmail.com>
2020-03-10 11:55:04 +0100

    Categories and Authors models and migrations created and updated

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Author extends Model
 {
-    //
+    // set fillable
+    protected $fillable = [
+        'name'
+    ];
+
+    // set guarded
+    protected $guarded = [
+        'created_at',
+        'id',
+        'updated_at'
+    ];
+
+    public function setNameAttribute($value) {
+        $this->attributes['name'] = trim(ucwords($value));
+    }
 }
--- database/migrations/2020_03_10_105341_create_categories_table.php
+++ database/migrations/2020_03_10_105341_create_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAuthorsTable extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,8 @@ class CreateAuthorsTable extends Migration
      */
     public function up()
     {
-        Schema::create('authors', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
             $table->timestamps();
         });
     }
@@ -27,6 +26,6 @@ class CreateAuthorsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('authors');
+        Schema::dropIfExists('categories');
     }
 }
